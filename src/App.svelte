283feff85121260052023958af405e2ea5c44543
enGuifre <guifre.jordan@gmail.com>
2023-03-30 01:25:36 +0200

    aaaaa

--- src/App.svelte
+++ src/App.svelte
@@ -1,32 +1,42 @@
 <script>
-  let header = "Enough phone for today!";
-</script>
-
-
-<style>
-	img {
-	  display: block;
-	  margin: auto;
-	  width: 100%;
-	  max-width: 1250px;
-	}
-	h2 {
-	  text-shadow: 10px 10px 10px #000;
-	  position: absolute;
-	  top: 5%;
-	  left: 35%;
-	  transform: translate(-60%, -50%);
-	  z-index: 1;
-	  color: white;
-	  font-size: 3em;
-	  text-align: center;
-	}
-  </style>
+	let header = "Enough phone for today!";
+  </script>
   
-  <div style="position:relative;">
-	<img src="photo 1.jpg" alt="Image"/>
-	<h2><b>{header}</b></h2>
-  </div>
+  
+  <style>
+	  img {
+		display: block;
+		margin: auto;
+		width: 100%;
+		max-width: 1250px;
+	  }
+	  img.smaller {
+		display: block;
+		margin: auto;
+		max-width: 500px;
+		top: 5%;
+		left: 35%;
+	  }
+	  h2 {
+		text-shadow: 10px 10px 10px #000;
+		position: absolute;
+		top: 2%;
+		left: 3%;
+		z-index: 1;
+		color: white;
+		font-size: 3em;
+		text-align: center;
+	  }
+	  p {
+		margin: 30px
+	  }
+	</style>
+	
+	<div style="position:relative;">
+	  <img src="photo 1.jpg" alt="Image"/>
+	  <br>
+	  <h2><b>{header}</b></h2>
+	</div>
 
 <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
 	Sed eget sapien vel purus molestie auctor eu sit amet risus. 
@@ -35,6 +45,10 @@
 	Nam quis metus a risus euismod vehicula. Duis bibendum volutpat ante, at semper nisi vehicula in. 
 	Nullam non diam id justo semper consectetur a non felis. 
 	Phasellus ac quam eu velit aliquam consequat nec vel orci. </p>
+
+	<img src="photo 2.jpg" alt="Image" class="smaller"/>
+
+	<br>
 	
 <img src="postits_4.png" alt="Image"/>
 <iframe src='https://flo.uri.sh/visualisation/13237030/embed' title='Interactive or visual content' class='flourish-embed-iframe' frameborder='0' scrolling='no' style='width:100%;height:600px;' sandbox='allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation'></iframe><div style='width:100%!;margin-top:4px!important;text-align:right!important;'><a class='flourish-credit' href='https://public.flourish.studio/visualisation/13237030/?utm_source=embed&utm_campaign=visualisation/13237030' target='_top' style='text-decoration:none!important'></a></div>
